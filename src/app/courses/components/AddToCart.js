bb991e4720ee171c082ae4b77860f889f43f3151
ALbertIM0427 <guang7226961@naver.com>
2024-10-13 23:47:13 +0900

    fix: cart 주소 수정

--- src/app/courses/components/AddToCart.js
+++ src/app/courses/components/AddToCart.js
@@ -4,19 +4,16 @@ import cookies from "js-cookie";
 
 const AddToCart = ({ id }) => {
   const addToCart = async () => {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_FRONTEND_URL}/api/cart`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${cookies.get("access_token")}`,
-        },
-        body: JSON.stringify({
-          course: id,
-        }),
-      }
-    );
+    const res = await fetch(`${process.env.NEXT_PUBLIC_FRONTEND_URL}/cart`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${cookies.get("access_token")}`,
+      },
+      body: JSON.stringify({
+        course: id,
+      }),
+    });
     if (res.ok) {
       window.location.href = "/cart";
     } else {
