be025200bdca111ad5802790d62fa0eca7657737
ALbertIM0427 <guang7226961@naver.com>
2024-10-15 00:38:27 +0900

    fix: kakaoPayButton checkout 수정

--- src/app/checkout/components/KakaoPayButton.js
+++ src/app/checkout/components/KakaoPayButton.js
@@ -1,31 +1,38 @@
 "use client";
 
-import { redirect } from "next/navigation";
-
 const KakaoPayButton = ({ access_token }) => {
   const api = process.env.NEXT_PUBLIC_FRONTEND_URL;
   const checkout = async (e) => {
     e.preventDefault();
 
-    const res = await fetch(`${api}/payments/`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${access_token}`,
-      },
-    })
-      .then((res) => {
-        if (res.ok) {
-          return res.json();
-        } else {
-          console.error("Error:", res);
-          alert("Failed to checkout");
-        }
-      })
-      .then((data) => {
-        console.log(data);
-        redirect(data.next_redirect_pc_url);
+    try {
+      const res = await fetch(`${api}/payments/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${access_token}`,
+        },
       });
+
+      if (!res.ok) {
+        console.error("Error:", res);
+        alert("Failed to checkout");
+        return;
+      }
+
+      const data = await res.json();
+      console.log(data);
+
+      // 리다이렉션을 위한 URL이 있다면 해당 URL로 이동
+      if (data.next_redirect_pc_url) {
+        window.location.href = data.next_redirect_pc_url;
+      } else {
+        alert("No redirect URL found");
+      }
+    } catch (error) {
+      console.error("Error during checkout:", error);
+      alert("An error occurred during the checkout process.");
+    }
   };
 
   return (
